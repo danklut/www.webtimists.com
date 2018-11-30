823dd080850a379dcd21e1e6167a9cce6485e97a
greglobinski <greglobinski@gmail.com>
2018-11-30 22:45:48 +0100

    feat: simple styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled, { injectGlobal } from 'react-emotion'
+import 'typeface-open-sans'
 
 injectGlobal`
-  * {
+  html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  *:after,
+  *:before {
+    box-sizing: inherit;
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+  }
+
+  body {
+    font-family:  'Open Sans', sans-serif;
   }
 `
 
@@ -18,11 +29,31 @@ const Container = styled('div')`
   width: 100%;
 `
 
+const Header = styled('h1')`
+  color: #666;
+  font-size: 2.1rem;
+  letter-spacing: -0.02em;
+  margin-bottom: 1rem;
+  text-align: center;
+`
+
+const Intro = styled('p')`
+  font-size: 1.2rem;
+  color: #333;
+  margin-bottom: 1rem;
+`
+
+const ContactLink = styled('a')`
+  color: #666;
+`
+
 const IndexPage = () => (
   <Container>
-    <h1>Webtimists Ltd.</h1>
-    <p>coming soon...</p>
-    <a href="mailto:hello@webtimists.com">hello@webtimists.com</a>
+    <Header>Webtimists Ltd.</Header>
+    <Intro>coming soon...</Intro>
+    <ContactLink href="mailto:hello@webtimists.com">
+      hello@webtimists.com
+    </ContactLink>
   </Container>
 )
 
