fdc4f858290c1b73c8ceef1c52d8873c6958f13a
greglobinski <greglobinski@gmail.com>
2018-11-30 22:01:56 +0100

    init git

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <div>
+    <h1>Webtimists Ltd.</h1>
+    <p>coming soon...</p>
+    <a href="mailto:hello@webtimists.com">hello@webtimists.com</a>
+  </div>
+)
+
+export default IndexPage
